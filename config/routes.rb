6f478ab726aa1250042bd3c28779c0c060391123
Alexia Sander <sander.alexia@gmail.com>
2023-07-06 21:34:11 +0900

    setup done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :flats
 end
+
+# flats GET       /flats(.:format)                       flats#index
+# POST            /flats(.:format)                       flats#create
+# new_flat GET    /flats/new(.:format)                   flats#new
+# edit_flat GET   /flats/:id/edit(.:format)              flats#edit
+# flat GET        /flats/:id(.:format)                   flats#show
+# PATCH           /flats/:id(.:format)                   flats#update
+# PUT             /flats/:id(.:format)                   flats#update
+# DELETE          /flats/:id(.:format)                   flats#destroy
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Destroying restaurants"
+Flat.destroy_all
+puts "Destroyed!"
+puts "Creating 5 flats"
+5.times do
+  flat = Flat.create( # create instead of new since create does save
+    name: Faker::Company.name,
+    address: Faker::Address.street_address,
+    description: Faker::Lorem.sentence,
+    price_per_night: Faker::Number.between(from: 50, to: 300),
+    number_of_guests: Faker::Number.between(from: 1, to: 6)
+    # class::constant.method
+  )
+  puts "#{flat.name} created."
+end
+puts "Done!"
